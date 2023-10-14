aa9e2f04c109e0a95b45b904cf13f6252e26fa2b
IlyaPoretskov <141244920+IlyaPoretskov@users.noreply.github.com>
2023-10-14 13:20:21 +0500

    Anagrams

---
+++ Anagrams/Anagrams.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <map>
+#include <string>
+
+using namespace std;
+
+bool is_anagrams(string first, string second)
+{
+    if (first.length() != second.length())     return false;
+
+    map <int, char>  firstMap;
+    map <int, char> secondMap;
+
+    for (int i = 0; i < first.length(); i++) {
+
+        firstMap.insert({first[i], first[i]});
+        secondMap.insert({second[i], second[i]});
+    }
+    if (firstMap != secondMap)    return false;
+    else  return true;
+}
+
+int main()
+{
+    string a, b;
+
+    cout << "Input first string:\n";
+    cin >> a;
+    cout << "Input second string:\n";
+    cin >> b;
+
+    cout << boolalpha << is_anagrams(a, b) << endl;
+
+    system("pause");
+    return 0;
+}
